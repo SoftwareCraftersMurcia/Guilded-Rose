f883fbd15ca269c842055b26343de9a085c32ef4
David Isla <d.isla@ibermatica.com>
2023-01-25 21:13:58 +0100

    Added usual files to GildedRose Kata

---
+++ kotlin/src/test/kotlin/gildedrose/GildedRoseTest.kt
@@ -0,0 +1,17 @@
+package gildedrose
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class GildedRoseTest {
+
+    @Test
+    fun foo() {
+        val items = arrayOf<Item>(Item("foo", 0, 0))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals("fixme", app.items[0].name)
+
+    }
+
+}
