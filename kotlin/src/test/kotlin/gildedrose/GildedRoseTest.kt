61d9718e476d48e4a0002f378cdd9516f954b77a
David Isla <d.isla@ibermatica.com>
2023-01-25 22:25:28 +0100

    Added approvals tests for Kotlin

--- kotlin/src/test/kotlin/gildedrose/GildedRoseTest.kt
+++ kotlin/src/test/kotlin/gildedrose/GildedRoseTest.kt
@@ -1,7 +1,9 @@
 package gildedrose
 
+import org.approvaltests.combinations.CombinationApprovals
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.lambda.utils.Range
 
 internal class GildedRoseTest {
 
@@ -11,7 +13,34 @@ internal class GildedRoseTest {
         val app = GildedRose(items)
         app.updateQuality()
         assertEquals("fixme", app.items[0].name)
+    }
 
+    @Test
+    fun approvalsTest() {
+        val names = arrayOf(
+            "+5 Dexterity Vest",
+            "Aged Brie",
+            "Elixir of the Mongoose",
+            "Sulfuras, Hand of Ragnaros",
+            "Backstage passes to a TAFKAL80ETC concert"
+        )
+        val sellIn = Range.get(-5, 15)
+        val quality = Range.get(0, 50)
+
+        CombinationApprovals.verifyAllCombinations(
+            this::updateQuality,
+            names,
+            sellIn,
+            quality
+        )
+
+    }
+
+    private fun updateQuality(name: String, sellIn: Int, quality: Int ) : String {
+        val items = arrayOf(Item(name, sellIn, quality))
+        val app = GildedRose(items)
+        app.updateQuality()
+        return items[0].toString()
     }
 
 }
