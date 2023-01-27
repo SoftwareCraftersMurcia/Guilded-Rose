3323c81c60fca39098d2d5fe232ead6de6bc11da
Pascual Cano <pascual@allhuman.com>
2023-01-27 19:07:58 +0100

    Replace all conditions with class methods

--- javascript/src/gilded_rose.js
+++ javascript/src/gilded_rose.js
@@ -5,34 +5,30 @@ class Shop {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (
-        this.isAgedBrie(this.items[i]) &&
-        this.isBackStagePasses(this.items[i])
+        !this.isAgedBrie(this.items[i]) &&
+        !this.isBackStagePasses(this.items[i])
       ) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+          if (!this.isSulfuras(this.items[i])) {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
+          if (this.isBackStagePasses(this.items[i])) {
             this.updateQualityForBackstagePasses(this.items[i]);
           }
         }
       }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+      if (!this.isSulfuras(this.items[i])) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != "Aged Brie") {
-          if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-          ) {
+          if (!this.isBackStagePasses(this.items[i])) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+              if (!this.isSulfuras(this.items[i])) {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
@@ -51,12 +47,16 @@ class Shop {
     return this.items;
   }
 
+  isSulfuras(item) {
+    return item.name == "Sulfuras, Hand of Ragnaros";
+  }
+
   isBackStagePasses(item) {
-    return item.name != "Backstage passes to a TAFKAL80ETC concert";
+    return item.name == "Backstage passes to a TAFKAL80ETC concert";
   }
 
   isAgedBrie(item) {
-    return item.name != "Aged Brie";
+    return item.name == "Aged Brie";
   }
 
   updateQualityForBackstagePasses(item) {
