8f93da978361bba145962b644969c1d62037d245
Pascual Cano <pascual@allhuman.com>
2023-01-27 19:16:33 +0100

    Refactor for

--- javascript/src/gilded_rose.js
+++ javascript/src/gilded_rose.js
@@ -5,46 +5,42 @@ class Shop {
     this.items = items;
   }
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (
-        !this.isAgedBrie(this.items[i]) &&
-        !this.isBackStagePasses(this.items[i])
-      ) {
-        if (this.items[i].quality > 0) {
-          if (!this.isSulfuras(this.items[i])) {
-            this.items[i].quality = this.items[i].quality - 1;
+    this.items.forEach((item) => {
+      if (!this.isAgedBrie(item) && !this.isBackStagePasses(item)) {
+        if (item.quality > 0) {
+          if (!this.isSulfuras(item)) {
+            item.quality = item.quality - 1;
           }
         }
       } else {
-        if (this.items[i].quality < MAX_QUALITY) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.isBackStagePasses(this.items[i])) {
-            this.updateQualityForBackstagePasses(this.items[i]);
+        if (item.quality < MAX_QUALITY) {
+          item.quality = item.quality + 1;
+          if (this.isBackStagePasses(item)) {
+            this.updateQualityForBackstagePasses(item);
           }
         }
       }
-      if (!this.isSulfuras(this.items[i])) {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+      if (!this.isSulfuras(item)) {
+        item.sellIn = item.sellIn - 1;
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (!this.isBackStagePasses(this.items[i])) {
-            if (this.items[i].quality > 0) {
-              if (!this.isSulfuras(this.items[i])) {
-                this.items[i].quality = this.items[i].quality - 1;
+      if (item.sellIn < 0) {
+        if (item.name != "Aged Brie") {
+          if (!this.isBackStagePasses(item)) {
+            if (item.quality > 0) {
+              if (!this.isSulfuras(item)) {
+                item.quality = item.quality - 1;
               }
             }
           } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
+            item.quality = item.quality - item.quality;
           }
         } else {
-          if (this.items[i].quality < MAX_QUALITY) {
-            this.items[i].quality = this.items[i].quality + 1;
+          if (item.quality < MAX_QUALITY) {
+            item.quality = item.quality + 1;
           }
         }
       }
-    }
+    });
 
     return this.items;
   }
